7ab68b94192a8827be0cbf25262a1cadac11110a
io42630 <ivan@olexyn.com>
2024-03-23 14:30:58 +0100

    + add TimeCube tests & fix some bugs

--- src/main/java/com/olexyn/TimeCube.java
+++ src/main/java/com/olexyn/TimeCube.java
@@ -6,29 +6,55 @@ import com.olexyn.min.entries.Pair;
 
 public class TimeCube {
 
-	private final int subSize = 65536;
+	public static final Instant START = Instant.parse("2015-01-01T00:00:00Z");
+
+	// This TimeQube is meant to track 10 years with a precision of 10ms.
+	// 10 years = 315569520000 ms
+	// 10ms precision = 31556952000 slots
+	// Root of 31556952000 = 177643 -> 200000
+
+	private static final int SUB_SIZE = 300000;
+
+
+	public TimeCube() {
+
+
+
+	}
+
 
 	@SuppressWarnings("unchecked")
-	private final Pair<Instant, Object>[][] arr = (Pair<Instant, Object>[][]) new Pair[65536][];
+	private final Pair<Instant, Object>[][] arr = (Pair<Instant, Object>[][]) new Pair[SUB_SIZE][];
 
-
-
-	public Pair<Instant, Object> get(int index) {
-		Pair<Instant, Object>[] subArr = arr[index / subSize];
-		if (subArr == null) {
-			arr[index / subSize] = (Pair<Instant, Object>[]) new Pair[subSize];
-		}
-		return arr[index / subSize][index % subSize];
+	private static long getIndex(Instant instant) {
+		return (instant.toEpochMilli() - START.toEpochMilli()) / 10;
 	}
 
-
-	public void put(int index,Pair<Instant, Object> value) {
-		Pair<Instant, Object>[] subArr = arr[index / subSize];
-		if (subArr == null) {
-			arr[index / subSize] = (Pair<Instant, Object>[]) new Pair[subSize];
-		}
-		arr[index / subSize][index % subSize] = value;
+	public Pair<Instant, Object> get(Instant instant) {
+		return get(getIndex(instant));
 	}
 
+	private Pair<Instant, Object> get(long index) {
+		var coords = getCoords(index);
+		return arr[coords[0]][coords[1]];
+	}
+
+	public void put(Instant instant, Object value) {
+		put(getIndex(instant), new Pair<>(instant, value));
+	}
+
+	private void put(long index, Pair<Instant, Object> value) {
+		var coords = getCoords(index);
+		arr[coords[0]][coords[1]] = value;
+	}
+
+	public int[] getCoords(long index) {
+		int aIdx = (int) (index / SUB_SIZE);
+		int bIdx = (int) (index % SUB_SIZE);
+		if (arr[aIdx] == null) {
+			arr[aIdx] = (Pair<Instant, Object>[]) new Pair[SUB_SIZE];
+		}
+		return new int[] { aIdx, bIdx};
+	}
 
 }
--- src/test/java/com/olexyn/TimeCubeTest.java
+++ src/test/java/com/olexyn/TimeCubeTest.java
@@ -1,9 +1,11 @@
 package com.olexyn;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.TreeMap;
 
 import com.olexyn.min.entries.Pair;
 import org.junit.BeforeClass;
@@ -18,13 +20,36 @@ import static org.junit.Assert.assertTrue;
 public class TimeCubeTest {
 
 
-    private static final List<Pair<String, String>> LIST = new ArrayList<>();
+    private  static final List<Pair<Instant, String>> MANY_KEYS = new ArrayList<>();
 
     @BeforeClass
     public static void setUp() {
 
+        for (int i = 0; i < 480000; i++) {
+            MANY_KEYS.add(new Pair<>(Instant.now().minusSeconds(i), "value " + i));
+        }
 
 
+    }
+
+    @Test
+    public void rangeTest() {
+
+        var timeCube = new TimeCube();
+        timeCube.put(TimeCube.START, "START");
+        assertEquals(timeCube.get(TimeCube.START).getB(), "START");
+
+        var now = Instant.now();
+        timeCube.put(now, "NOW");
+        assertEquals(timeCube.get(now).getB(), "NOW");
+
+        var inFiveYears = Instant.now().plus(Duration.ofDays(365 * 5));
+        timeCube.put(inFiveYears, "inFiveYears");
+        assertEquals(timeCube.get(inFiveYears).getB(), "inFiveYears");
+
+        var inTenYears = Instant.now().plus(Duration.ofDays(365 * 10));
+        timeCube.put(inTenYears, "inTenYears");
+        assertEquals(timeCube.get(inTenYears).getB(), "inTenYears");
     }
 
     @Test
@@ -41,9 +66,111 @@ public class TimeCubeTest {
         for ( var instant : keys ) {
             int index = new Random().nextInt(Integer.MAX_VALUE);
             String value = "value " + index;
-            arrList.put(index, new Pair<>(instant, value));
-			assertEquals(arrList.get(index).getB(), value);
+            arrList.put(instant, value);
+			assertEquals(arrList.get(instant).getB(), value);
         }
+
+    }
+
+    @Test
+    public void putPerformanceTest() {
+
+            var timeCube = new TimeCube();
+
+            var start = Instant.now();
+
+            for ( var pair : MANY_KEYS ) {
+                timeCube.put(pair.getA(), pair.getB());
+            }
+
+            var end = Instant.now();
+
+            System.out.println("TimeCube x1 : " + (end.toEpochMilli() - start.toEpochMilli())  + " ms");
+
+            var treeMap = new TreeMap<Instant, String>();
+            start = Instant.now();
+            for ( var pair : MANY_KEYS ) {
+                treeMap.put(pair.getA(), pair.getB());
+            }
+            end = Instant.now();
+
+            System.out.println("TreeMap x1 : " + (end.toEpochMilli() - start.toEpochMilli()) + " ms");
+
+            int putLoops = 1000;
+
+            start = Instant.now();
+            for ( var i = 0; i < putLoops; i++ ) {
+                for ( var pair : MANY_KEYS ) {
+                    assertEquals(timeCube.get(pair.getA()).getB(), pair.getB());
+                }
+            }
+            end = Instant.now();
+            System.out.println("TimeCube x" + putLoops + " : " + (end.toEpochMilli() - start.toEpochMilli()) + " ms");
+
+            start = Instant.now();
+            for ( var i = 0; i < putLoops; i++ ) {
+                for ( var pair : MANY_KEYS ) {
+                    assertEquals(treeMap.get(pair.getA()), pair.getB());
+                }
+            }
+            end = Instant.now();
+            System.out.println("TreeMap x" + putLoops + " : " + (end.toEpochMilli() - start.toEpochMilli()) + " ms");
+
+    }
+
+    @Test
+    public void getPerformanceTest() {
+        int getLoops = 1000;
+
+        // Prepare TimeCube
+        var timeCube = new TimeCube();
+        for ( var pair : MANY_KEYS ) {
+            timeCube.put(pair.getA(), pair.getB());
+        }
+        // Prepare TreeMap
+        var treeMap = new TreeMap<Instant, String>();
+
+        for ( var pair : MANY_KEYS ) {
+            treeMap.put(pair.getA(), pair.getB());
+        }
+
+        // Test TimeCube X1
+        var start = Instant.now();
+        for ( var pair : MANY_KEYS ) {
+            assertEquals(timeCube.get(pair.getA()).getB(), pair.getB());
+        }
+        var end = Instant.now();
+        System.out.println("TimeCube get x1 : " + (end.toEpochMilli() - start.toEpochMilli()) + " ms");
+        // Test TreeMap X1
+        start = Instant.now();
+        for ( var pair : MANY_KEYS ) {
+            assertEquals(treeMap.get(pair.getA()), pair.getB());
+        }
+        end = Instant.now();
+        System.out.println("TreeMap get x1 : " + (end.toEpochMilli() - start.toEpochMilli()) + " ms");
+
+        // Test TimeCube X100
+        start = Instant.now();
+        for ( var i = 0; i < getLoops; i++ ) {
+            for ( var pair : MANY_KEYS ) {
+                assertEquals(timeCube.get(pair.getA()).getB(), pair.getB());
+            }
+        }
+        end = Instant.now();
+        System.out.println("TimeCube get x" + getLoops + " : " + (end.toEpochMilli() - start.toEpochMilli()) + " ms");
+
+
+        // Test TreeMap X100
+        start = Instant.now();
+        for ( var i = 0; i < getLoops; i++ ) {
+            for ( var pair : MANY_KEYS ) {
+                assertEquals(treeMap.get(pair.getA()), pair.getB());
+            }
+        }
+        end = Instant.now();
+        System.out.println("TreeMap get x" + getLoops + " : " + (end.toEpochMilli() - start.toEpochMilli()) + " ms");
+
+
 
     }
 
